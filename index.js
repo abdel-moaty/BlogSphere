02e676eea10db183f958878c1c8eb46aa23ce48c
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2024-03-20 23:38:30 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// index.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
